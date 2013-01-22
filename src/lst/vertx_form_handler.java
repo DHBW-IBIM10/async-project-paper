e5d843ab17edcc8f7fa6c9416b8c9bbbfc8bb836
Rocco Schulz <schocco.rulz@googlemail.com>
2013-01-22 16:15:23 +0100

    Added bridge information in vert.x part (needed because its used in the implementation part)
    updated listing.

--- src/lst/vertx_form_handler.java
+++ src/lst/vertx_form_handler.java
@@ -1,18 +1,22 @@
-	private Handler<HttpServerRequest> formHandler = new Handler<HttpServerRequest>() {
-		public void handle(final HttpServerRequest req) {
-			req.bodyHandler(new Handler<Buffer>() {
-				
-				@Override
-				public void handle(Buffer buff) {
-					JsonArray form = new JsonArray(buff.toString());
-
-					eBus.send("form.calculate", form,
-							new Handler<Message<JsonArray>>() {
-								public void handle(Message<JsonArray> message) {
-									req.response.end(message.body.toString());
-								}
-							});
-				}
-			});
-		}
-	};
+/**
+ * Triggers the fee calculation based on the given form data.
+ */
+private Handler<HttpServerRequest> calcHandler = new Handler<HttpServerRequest>() {
+	public void handle(final HttpServerRequest req) {
+		// read body of request
+		req.bodyHandler(new Handler<Buffer>() {
+			@Override
+			public void handle(Buffer buff) {
+				JsonObject form = new JsonObject(buff.toString());
+				// trigger calculation in worker verticle
+				eBus.send("form.calculate", form,
+				new Handler<Message<JsonObject>>() {
+					public void handle(Message<JsonObject> message) {
+						req.response.headers().put("Content-Type", "application/json; charset=utf-8");
+						req.response.end(message.body.encode());
+					}
+				});
+			}
+		});
+	}
+};
